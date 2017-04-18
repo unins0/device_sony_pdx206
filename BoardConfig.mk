4b717dca3d365269043583bbaa24a9a8e938bd11
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    pdx206: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,4 +41,4 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # inherit from the proprietary version
--include vendor/sony/pdx206/BoardConfigVendor.mk
+include vendor/sony/pdx206/BoardConfigVendor.mk
